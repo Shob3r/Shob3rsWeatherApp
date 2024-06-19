80761bef88677c64c06b5b9276d4df45730d1bec
Shob3r <andrewpinter23@gmail.com>
2024-06-19 10:07:36 -0700

    Migrate to OpenWeatherMap one call v3 for future forecasts

--- util/OpenWeatherFutureForecasting.cs
+++ util/OpenWeatherFutureForecasting.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Shob3rsWeatherApp.Util;
 
 namespace Shob3rsWeatherApp;
-
 public class OpenWeatherFutureForecasting : OpenWeatherData
 {
     public readonly List<string> futureTemperatures = [];
@@ -12,27 +10,12 @@ public class OpenWeatherFutureForecasting : OpenWeatherData
 
     public override async Task updateWeatherData()
     {
-        // Migrate to OneCall V3 for future forecasting if I have the time for it
-        JsonParser weatherParser = new JsonParser(await HttpUtils.getHttpContent($"https://api.openweathermap.org/data/2.5/forecast?q={LocationInformation.currentCity},{LocationInformation.countryOfResidence}&units={getMeasurementSystem()}&appid={Env.openWeatherKey}"));
+        JsonParser weatherParser = new JsonParser(await HttpUtils.getHttpContent($"https://api.openweathermap.org/data/3.0/onecall?lat={LocationInformation.latitude}&lon={LocationInformation.longitude}&exclude=current,minutely,hourly,alerts&units=metric&appid={Env.openWeatherKey}"));
 
-        // Cursed for loop conditions
-        for (int i = 7; i < 40; i += 8)
+        for (int i = 0; i < 4; i++)
         {
-            // When asking OpenWeatherMap API v2.5 about the weather in the future, it returns weather data in the future in three hour increments.
-            // I only want to show the weather once every 24 hours, and start 24 hours from now.
-            // So we start at element 7 of the array, which is 24 hours from when the application runs, and then increment it by 8 each time from there on
-            // As 8 segments of 3 hour increments = 24 hours
-            
-            try
-            {
-                futureTemperatures.Add(roundTemp(weatherParser.getDataByTag<float>($"list[{i}].main.temp")).ToString());
-                futureWeatherDescriptions.Add(weatherParser.getDataByTag<string>($"list[{i}].weather[0].main"));
-            }
-            catch (IndexOutOfRangeException e)
-            {
-                Console.WriteLine($"oops\n{e}");
-                throw;
-            }
+            futureTemperatures.Add(roundTemp(weatherParser.getDataByTag<float>($"daily[{i}].temp.max")).ToString());
+            futureWeatherDescriptions.Add(weatherParser.getDataByTag<string>($"daily[{i}].weather[0].main"));
         }
     }
 }
